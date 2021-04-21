cd086be6684e391fc147574e147ed8cb2b0a8278
Tim Haak <tim@haak.co>
2021-04-21 13:18:40 +0200

    Added compression to assets. Added settings to speed up build

--- Dockerfile
+++ Dockerfile
@@ -18,7 +18,7 @@ COPY . ./
 RUN npm set progress=false
 RUN npm --unsafe-perm install --prefer-offline
 RUN npx browserslist@latest --update-db
-RUN npm run build --parallel --cache --hardSource
+RUN npm run build
 
 
 EXPOSE 3000/tcp
--- dockerBaseBuild.sh
+++ dockerBaseBuild.sh
@@ -2,12 +2,17 @@
 export IMAGE_NAME="${IMAGE_NAME:-haakco/phpsa-website-nuxt}"
 export DOCKER_FILE="${DOCKER_FILE:-Dockerfile}"
 export DO_DOCKER_PUSH="${DO_DOCKER_PUSH:-FALSE}"
+export RUN_BUILDX_SETUP="${RUN_BUILDX_SETUP:-FALSE}"
+export BUILDX_NAME="${BUILDX_NAME:-phpsa}"
 
-docker context create phpsa
-docker buildx create phpsa --name phpsa --driver docker-container --use
-docker buildx use phpsa
-#docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-#docker run --privileged --rm tonistiigi/binfmt --install all
+if [[ "${RUN_BUILDX_SETUP}" = "TRUE" ]]; then
+  docker context create "${BUILDX_NAME}"
+  docker buildx create "${BUILDX_NAME}" --name "${BUILDX_NAME}" --driver docker-container --use
+  docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+  docker run --privileged --rm tonistiigi/binfmt --install all
+fi
+
+docker buildx use "${BUILDX_NAME}"
 docker buildx inspect --bootstrap
 
 CMD='docker buildx build --load '"${DOCKER_BUILD_TYPE}"' --platform  linux/arm/v7,linux/arm64,linux/amd64 --tag '"${IMAGE_NAME}"' .'
